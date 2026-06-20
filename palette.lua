c59d60832c4a101b7cea3584fc05bdc09a1da31b
ThinhNguyenHoang <hoangthinhkg333@gmail.com>
2026-06-20 13:45:11 +0700

    fix treesitter variables

--- palette.lua
+++ palette.lua
@@ -33,6 +33,44 @@ local purple = "#5c21a5"
 local white = "#ffffff"
 local cyan = "#007872"
 
+-- Instead of RGB where you specify red, green and blue components, HSL uses:
+--
+-- Hue        (0 - 360) (each value is a angle around the color wheel)
+-- Saturation (0 - 100) (0 is gray, 100 is colored)
+-- Lightness  (0 - 100) (0 is black, 100 is white)
+-- Note: Converting between colorspaces can introduce minor color differences
+--       due to floating point maths. You may prefer to manually adjust your
+--       colors "by eye" afterwards.
+
+-- ###
+-- ### HSL operations
+-- ###
+--
+-- Lush.hsl (and hsluv) provides a number of convenience functions for:
+--
+--   Relative adjustment (rotate(), saturate(), desaturate(), lighten(), darken())
+--   Absolute adjustment (prefix above with abs_)
+--   Combination         (mix())
+--   Overrides           (hue(), saturation(), lightness())
+--   Access              (.h, .s, .l)
+--   Coercion            (tostring(), "Concatination: " .. color)
+--   Helpers             (readable())
+--
+--   Adjustment functions have shortcut aliases, ro, sa, de, li, da
+--                                               abs_sa, abs_de, abs_li, abs_da
+--
+-- Because HSL colors are represented by degrees around a colorwheel, we can find
+-- harmonious colors from our original set by rotating the hue:
+
+local hsl = lush.hsl -- We'll use hsl a lot so its nice to bind it separately
+local water = hsl(208, 100, 80) -- Vim has a mapping, <n>C-a and <n>C-x to
+local water_deep = hsl(208, 90, 30) -- increment or decrement integers, or
+local water_abyss = hsl(146, 90, 10) -- you can just type them normally.
+local sea_gull = hsl("#efefef")
+
+local leaf = hsl(150, 100, 30)
+local wood = hsl(351, 100, 37)
+
 g.terminal_color_0 = black
 g.terminal_color_1 = red
 g.terminal_color_2 = green
@@ -476,45 +514,12 @@ local base = {
 	yardComment = { link = "Comment" },
 	yardType = { link = "Todo" },
 	yardTypeList = { link = "Todo" },
+	["variable"] = { fg = black },
+	["@variable"] = { fg = black },
+	["@keyword"] = { fg = wood },
+	["@operator"] = { fg = black },
+	["@tag"] = { fg = black },
 }
-
--- Instead of RGB where you specify red, green and blue components, HSL uses:
---
--- Hue        (0 - 360) (each value is a angle around the color wheel)
--- Saturation (0 - 100) (0 is gray, 100 is colored)
--- Lightness  (0 - 100) (0 is black, 100 is white)
--- Note: Converting between colorspaces can introduce minor color differences
---       due to floating point maths. You may prefer to manually adjust your
---       colors "by eye" afterwards.
-
--- ###
--- ### HSL operations
--- ###
---
--- Lush.hsl (and hsluv) provides a number of convenience functions for:
---
---   Relative adjustment (rotate(), saturate(), desaturate(), lighten(), darken())
---   Absolute adjustment (prefix above with abs_)
---   Combination         (mix())
---   Overrides           (hue(), saturation(), lightness())
---   Access              (.h, .s, .l)
---   Coercion            (tostring(), "Concatination: " .. color)
---   Helpers             (readable())
---
---   Adjustment functions have shortcut aliases, ro, sa, de, li, da
---                                               abs_sa, abs_de, abs_li, abs_da
---
--- Because HSL colors are represented by degrees around a colorwheel, we can find
--- harmonious colors from our original set by rotating the hue:
-
-local hsl = lush.hsl -- We'll use hsl a lot so its nice to bind it separately
-local water = hsl(208, 100, 80) -- Vim has a mapping, <n>C-a and <n>C-x to
-local water_deep = hsl(208, 90, 30) -- increment or decrement integers, or
-local water_abyss = hsl(146, 90, 10) -- you can just type them normally.
-local sea_gull = hsl("#efefef")
-
-local leaf = hsl(150, 100, 30)
-local wood = hsl(351, 100, 37)
 
 ---@diagnostic disable: undefined-global
 local theme = lush(function()
@@ -543,6 +548,3 @@ for k, v in pairs(theme) do
 end
 
 return base
--- for group, opts in pairs(base) do
--- 	api.nvim_set_hl(0, group, opts)
--- end
